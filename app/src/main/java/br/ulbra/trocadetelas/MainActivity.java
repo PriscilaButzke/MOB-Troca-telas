3157d5729b1e1e03eb05e2b2b0ce3867a2c2cc5f
PriscilaButzke <priscilabutzke@outlook.com>
2022-10-06 19:32:27 -0300

    first commit

---
+++ app/src/main/java/br/ulbra/trocadetelas/MainActivity.java
@@ -0,0 +1,42 @@
+package br.ulbra.trocadetelas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.*;
+import android.view.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bttelaprincipal, bttela2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CarregarTelaPrincipal();
+    }
+
+    public void CarregarTelaPrincipal() {
+        setContentView(R.layout.activity_main);
+        bttela2 = (Button) findViewById(R.id.bttela2);
+        bttela2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CarregarTela2();
+            }
+        });
+    }
+
+    public void CarregarTela2() {
+        setContentView(R.layout.tela2);
+        bttelaprincipal = (Button) findViewById(R.id.bttelaprincipal);
+        bttelaprincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CarregarTelaPrincipal();
+            }
+        });
+    }
+}
